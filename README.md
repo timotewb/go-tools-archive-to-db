64069d5414d996051c5bf138b2eb81a5846187aa
timotewb <timote.wb@gmail.com>
2024-01-12 09:22:28 +1300

    first build

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # go-tools-archive-to-db
+
 List files in directory and write metadata to database
+
+GOOS=darwin GOARCH=arm64 go build -o build/go-tools-archive-to-db
+
+set GOOS=windows ; set GOARCH=amd64 ; go build -o build/go-tools-archive-to-db.exe
+
+# todos
--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
+	"unicode"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/ncruces/zenity"
 )
 
 type FileType struct {
@@ -19,95 +22,224 @@ type FileType struct {
 }
 
 func main(){
-	// inDir, err := zenity.SelectFile(
-	// 	zenity.Filename(""),
-	// 	zenity.Directory(),
-	// 	zenity.DisallowEmpty(),
-	// 	zenity.Title("Select input directory."),
-	// )
-	// if err != nil {
-	// 	zenity.Error(
-	// 		err.Error(),
-	// 		zenity.Title("Error"),
-	// 		zenity.ErrorIcon,
-	// 	)
-	// 	log.Fatal(err)
-	// }
+	inDir, err := zenity.SelectFile(
+		zenity.Filename(""),
+		zenity.Directory(),
+		zenity.DisallowEmpty(),
+		zenity.Title("Select input directory."),
+	)
+	if err != nil {
+		zenity.Error(
+			err.Error(),
+			zenity.Title("Error"),
+			zenity.ErrorIcon,
+		)
+		log.Fatal(err)
+	}
 
-	// dev only
-	inDir := "/Volumes/ns01/users/timotewb/"
 	dbFile := "db.sqlite"
-	fmt.Println(inDir)
+	tableName := makeValidTableName(filepath.Base(inDir))
+	fmt.Println("--------------------------------------------------")
+	fmt.Println("Start processing for:")
+	fmt.Printf(" - %s\n", inDir)
+	fmt.Printf(" - %s\n", dbFile)
+	fmt.Printf(" - %s\n", tableName)
+	fmt.Println("--------------------------------------------------")
 
-	searchFiles(inDir, dbFile)
 
+	// setup database
+	// check if database exists
+	db, err := sql.Open("sqlite3", dbFile)
+	if err != nil {
+		zenity.Error(
+			err.Error(),
+			zenity.Title("Error"),
+			zenity.ErrorIcon,
+		)
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// drop table if exists
+	_, err = db.Exec("DROP TABLE IF EXISTS "+tableName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec("DROP TABLE IF EXISTS "+tableName+"_err")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a table if it doesn't exist.
+	stmt, err := db.Prepare(`CREATE TABLE `+tableName+` (
+		id INTEGER PRIMARY KEY,
+		name TEXT NOT NULL,
+		extension TEXT NOT NULL,
+		path TEXT NOT NULL,
+		size INTEGER NOT NULL
+	)`)
+	if err != nil {
+		zenity.Error(
+			err.Error(),
+			zenity.Title("Error"),
+			zenity.ErrorIcon,
+		)
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec()
+	if err != nil {
+		zenity.Error(
+			err.Error(),
+			zenity.Title("Error"),
+			zenity.ErrorIcon,
+		)
+		log.Fatal(err)
+	}
+
+	// Create a table if it doesn't exist.
+	stmt, err = db.Prepare(`CREATE TABLE `+tableName+`_err (
+		id INTEGER PRIMARY KEY,
+		file TEXT NOT NULL,
+		message TEXT NOT NULL
+	)`)
+	if err != nil {
+		zenity.Error(
+			err.Error(),
+			zenity.Title("Error"),
+			zenity.ErrorIcon,
+		)
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec()
+	if err != nil {
+		zenity.Error(
+			err.Error(),
+			zenity.Title("Error"),
+			zenity.ErrorIcon,
+		)
+		log.Fatal(err)
+	}
+
+	// Search files
+	searchFiles(inDir, dbFile, tableName, db)
+
+	// Complete messages
+	zenity.Info("Folder search complete!",
+		zenity.Title("Complete"),
+		zenity.InfoIcon,
+	)
 
 }
 
-func searchFiles(inDir string, dbFile string){
+func searchFiles(inDir string, dbFile string, tableName string, db *sql.DB){
+
+	fmt.Println(inDir)
+
 	files, err := os.ReadDir(inDir)
 	if err != nil {
+		zenity.Error(
+			err.Error(),
+			zenity.Title("Error"),
+			zenity.ErrorIcon,
+		)
 		log.Fatal(err)
 	}
 
 	for _, f := range files {
 		if f.IsDir(){
 			// run function
-			fmt.Println(f.Name())
-			searchFiles(filepath.Join(inDir, f.Name()), dbFile)
+			searchFiles(filepath.Join(inDir, f.Name()), dbFile, tableName, db)
 
 		} else {
-			writeToDB(inDir, dbFile, f)
+			writeToDB(inDir, dbFile, tableName, f, db)
 		}
 	}
 }
 
-func writeToDB(inDir string, dbFile string, f fs.DirEntry){
+func writeToDB(inDir string, dbFile string, tableName string, f fs.DirEntry, db *sql.DB){
 
-			// check if database exists
-			db, err := sql.Open("sqlite3", dbFile)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer db.Close()
+	fileInfo, err := os.Stat(filepath.Join(inDir, f.Name()))
+	if err != nil {
+		errorDB(tableName+"_err", filepath.Join(inDir, f.Name()), err.Error(), db)
+		return
+	}
 
-			// Create a table if it doesn't exist.
-			stmt, err := db.Prepare(`CREATE TABLE IF NOT EXISTS files (
-				id INTEGER PRIMARY KEY,
-				name TEXT NOT NULL,
-				extension TEXT NOT NULL,
-				path TEXT NOT NULL,
-				size INTEGER NOT NULL
-			)`)
-			if err != nil {
-				log.Fatal(err)
-			}
-			_, err = stmt.Exec()
-			if err != nil {
-				log.Fatal(err)
-			}
+	// define and set data
+	file := FileType{
+		Name: f.Name(),
+		Extension: filepath.Ext(f.Name()),
+		Path: inDir,
+		Size: fileInfo.Size(),
+	}
 
-			fileInfo, err := os.Stat(filepath.Join(inDir, f.Name()))
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			file := FileType{
-				Name: f.Name(),
-				Extension: filepath.Ext(f.Name()),
-				Path: inDir,
-				Size: fileInfo.Size(),
-			}
-
-			// Prepare the SQL statement.
-			stmt, err = db.Prepare("INSERT INTO files (name, extension, path, size) VALUES (?, ?, ?, ?)")
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// Execute the statement.
-			_, err = stmt.Exec(file.Name, file.Extension, file.Path, file.Size)
-			if err != nil {
-				log.Fatal(err)
-			}
+	// insert into db
+	stmt, err := db.Prepare("INSERT INTO "+tableName+" (name, extension, path, size) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		zenity.Error(
+			err.Error(),
+			zenity.Title("Error"),
+			zenity.ErrorIcon,
+		)
+		log.Fatal(err)
+	}
+	// Execute the statement.
+	_, err = stmt.Exec(file.Name, file.Extension, file.Path, file.Size)
+	if err != nil {
+		zenity.Error(
+			err.Error(),
+			zenity.Title("Error"),
+			zenity.ErrorIcon,
+		)
+		log.Fatal(err)
+	}
 }
+
+func makeValidTableName(str string) string {
+	// Replace invalid characters with underscore
+	reg := regexp.MustCompile(`[^a-zA-Z0-9_]`)
+	str = reg.ReplaceAllString(str, "_")
+
+	// Ensure the first character is a letter
+	if !unicode.IsLetter(rune(str[0])) {
+		str = "t_" + str
+	}
+
+	return str
+}
+
+func errorDB(tableName string, fileNamePath string, errMsg string, db *sql.DB){
+	stmt, err := db.Prepare("INSERT INTO "+tableName+" (file, message) VALUES (?, ?)")
+	if err != nil {
+		zenity.Error(
+			err.Error(),
+			zenity.Title("Error"),
+			zenity.ErrorIcon,
+		)
+		log.Fatal(err)
+	}
+	// Execute the statement.
+	_, err = stmt.Exec(fileNamePath, errMsg)
+	if err != nil {
+		zenity.Error(
+			err.Error(),
+			zenity.Title("Error"),
+			zenity.ErrorIcon,
+		)
+		log.Fatal(err)
+	}
+}
+
+// select
+// 	t1.size,
+// 	t1.name as t1name,
+// 	t2.name as t2name,
+// 	t1.path as t1path,
+// 	t2.path as t2path
+// from
+// 	ns01 t1
+// 	inner join ns01 t2
+// 		on(t1.extension = t2.extension
+// 			and t1.size = t2.size
+// 			and t1.path <> t2.path)
+// order by
+// 	t1.size desc
